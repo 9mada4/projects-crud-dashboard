aa9e0da9104d26bf310f0e45cf2968f69ae66e7f
Kumamoto Seita <135863869+9mada4@users.noreply.github.com>
2025-06-20 16:51:24 +0900

    chore: update TypeScript ESLint packages and add eslint-plugin-react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,11 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': 'warn',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
+  rules: {},
 }
